a4c13a39c900a29b3d4400a6ff734684ae735011
Marlon O'Neil <hello@marlononeil.com>
2017-08-23 16:10:46 -0400

    tried to add html links to favourites section

--- server.rb
+++ server.rb
@@ -8,3 +8,24 @@ end
 get '/portfolio' do
   erb :gallery
 end
+
+get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
+  erb :about_me
+end
+
+
+get '/favourites' do
+  @fav_links = ['www.google.com', 'www.bitmaker.co', 'www.outlook.com', 'www.bbc.com', 'www.cnn.com']
+  erb :favourites
+end
+
+
+get '/' do
+  redirect to('/home')
+end
+
+get '/gallery' do
+  redirect to('/portfolio')
+end
